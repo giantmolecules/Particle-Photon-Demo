e67ad7f60491d16d2dfab4df68e7f3734d2db9c0
Brett Ian Balogh <brettbalogh@gmail.com>
2017-01-10 06:59:36 -0600

    init

---
+++ Assets/CallPhotonFunction.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Experimental.Networking;
+
+public class CallPhotonFunction: MonoBehaviour {
+
+	public string deviceID;
+	public string accessToken;
+	private string requestURL;
+	private string func;
+	private Renderer theRenderer;
+
+	void Start() {
+		Debug.Log ("START");
+		func = "ledSwitch";
+		requestURL = "https://api.spark.io/v1/devices/" + deviceID + "/" + func + "/";
+		theRenderer = GetComponent<Renderer> ();
+		theRenderer.material.color = new Color (1f, 1f, 1f);
+	}
+
+	void OnTriggerEnter(Collider other){
+		Debug.Log ("ENTER");
+		StartCoroutine(Upload(1));
+		theRenderer.material.color = new Color (0f, 0f, 1f);
+	}
+
+	void OnTriggerExit(Collider other){
+		Debug.Log ("EXIT");
+		StartCoroutine(Upload(0));
+		theRenderer.material.color = new Color (1f, 1f, 1f);
+	}
+
+	IEnumerator Upload(int value) {
+		WWWForm form = new WWWForm();
+		form.AddField("newValue", value.ToString());
+		form.AddField("access_token", accessToken);
+		UnityEngine.Networking.UnityWebRequest www = UnityEngine.Networking.UnityWebRequest.Post(requestURL, form);
+		yield return www.Send();
+
+		if(www.isError) {
+			Debug.Log(www.error);
+		}
+		else {
+			Debug.Log("Form upload complete!");
+		}
+	}
+}
---
+++ Assets/GetPhotonVariable.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net.Sockets;
+using UnityEngine.Networking;
+using Particle.SDK;
+using System.Threading;
+
+public class GetPhotonVariable: MonoBehaviour {
+
+	public string deviceID;
+	public string accessToken;
+	public string variableName;
+	private string result;
+	private bool status = ParticleCloud.SharedCloud.LoginAsync("giantmolecules@gmail.com", "c0t1onic");
+
+	void Start() {
+		InvokeRepeating("startRepeating", 1, 5.0F);
+	}
+
+	void startRepeating(){
+		StartCoroutine(GetText());
+	}
+
+	IEnumerator GetText() {
+		UnityWebRequest www = UnityWebRequest.Get("https://api.particle.io/v1/devices/" + deviceID + "/" + variableName + "?access_token=" + accessToken);
+		yield return www.Send();
+
+		if(www.isError) {
+			Debug.Log ("Error:");
+			Debug.Log(www.error);
+		}
+		else {
+			// Show results as text
+			Debug.Log("RESULTS");
+			Debug.Log(JsonUtility.FromJson<string>(www.downloadHandler.text));
+			//result = www.ser;
+			//Debug.Log (result);
+
+			// Or retrieve results as binary data
+			//byte[] results = www.downloadHandler.data;
+			//Debug.Log (results.ToString());
+			//Debug.Log(results.);
+			;
+		}
+	}
+}
